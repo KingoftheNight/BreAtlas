54873462be88ff92c08b3739eed1e0e17fdf1607
Liang YuChao <43328497+KingoftheNight@users.noreply.github.com>
2022-03-10 16:52:59 +0800

    Update Plot.py

--- breatlas/Plot.py
+++ breatlas/Plot.py
@@ -3,6 +3,28 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib
 
+def plot_ifs(data, type_p, out):
+    x = []
+    y = []
+    for i in range(len(data)):
+        x.append(i + 1)
+    for j in data:
+        y.append(j)
+    plt.figure()
+    plt.plot(x, y, label='ACC')
+    plt.xlabel("Feature Number")
+    plt.ylabel("Acc")
+    plt.title(type_p)
+    max_x = y.index(max(y))
+    max_y = max(y)
+    plt.text(max_x, max_y, str(max_x + 1) + '(' + str(max_y * 100) + '%)', fontsize=10)
+    # 保存图片
+    png1 = io.BytesIO()
+    plt.savefig(png1, format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
+    png2 = Image.open(png1)
+    png2.save(out+".tiff")
+    png1.close()
+    
 def plot_bar(pc1_featurescore, out, number=10):
     # 中文乱码和坐标轴负号处理
     matplotlib.rc('font', family='SimHei', weight='bold')
